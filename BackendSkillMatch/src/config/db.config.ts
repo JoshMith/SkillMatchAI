0515b9bdebd33e40049c038cd7469e3cf41848d0
JoshMith <150962666+JoshMith@users.noreply.github.com>
2025-04-23 23:16:15 +0300

    Disabled SSL connection

--- BackendSkillMatch/src/config/db.config.ts
+++ BackendSkillMatch/src/config/db.config.ts
@@ -1,7 +1,12 @@
-import { Pool } from 'pg'
-import dotenv from 'dotenv'
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+import { ConnectionOptions } from 'tls';
 
-dotenv.config()
+dotenv.config();
+
+const ssl: ConnectionOptions = {
+    rejectUnauthorized: false, // for development (set true in production with valid cert)
+};
 
 const pool = new Pool({
     host: process.env.DB_HOST,
@@ -9,6 +14,7 @@ const pool = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-})
+    ssl,
+});
 
-export default pool
+export default pool;
